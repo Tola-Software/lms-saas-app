c8360555e4c38ba8ad42445feda85a876e8eeb3f
Mocha Adam Fadly <adamfadly11@gmail.com>
2025-05-28 20:59:01 +0800

    billing, supabase

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import CompanionForm from "@/components/CompanionForm";
+import {auth} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
 
-const page = () =>
-  <main className="lg:w-1/3 md:w-2/3 items-center justify-center">
-    <article className="w-full gap-4 flex flex-col">
-      <h1>Companion Builder</h1>
-      <CompanionForm/>
-    </article>
-  </main>
+const page = async () => {
+  const {userId} = await auth();
+  if (!userId) redirect('/sign-in');
+  return (
+    <main className="lg:w-1/3 md:w-2/3 items-center justify-center">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+        <CompanionForm/>
+      </article>
+    </main>
+  )
+}
 export default page;
