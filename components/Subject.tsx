3fd01f837030762aba45656c1043135640f40d8f
Mocha Adam Fadly <adamfadly11@gmail.com>
2025-05-29 18:41:10 +0800

    comapion detail by id

---
+++ components/Subject.tsx
@@ -0,0 +1,66 @@
+'use client'
+import React, {useEffect, useState} from 'react';
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {subjects} from "@/lib/constants";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {formUrlQuery, removeKeysFromUrlQuery} from "@jsmastery/utils";
+
+const SubjectFilter = () => {
+
+  const pathname = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const query = searchParams.get('topic')
+
+  const [subject, setSubject] = useState('')
+
+  useEffect(() => {
+  if(subject) {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: 'subject',
+      value: subject,
+    })
+    router.push(newUrl,{scroll: false})
+  }else {
+    if(subject === 'all'){
+      const newUrl = removeKeysFromUrlQuery({
+        params: searchParams.toString(),
+        keysToRemove: ['subject']
+      })
+      router.push(newUrl, {scroll: false})
+    }
+  }
+  },  [subject, router, searchParams, pathname]);
+
+
+  return (
+
+    <section>
+      <Select
+        onValueChange={setSubject}
+        value={subject}
+        defaultValue="all"
+      >
+        <SelectTrigger className="input capitalize">
+          <SelectValue
+            placeholder="Subject"
+          />
+        </SelectTrigger>
+        <SelectContent>
+
+          <SelectItem value="all">
+            All Subjects
+          </SelectItem>
+          {subjects.map((subject, index) => (
+            <SelectItem value={subject} key={index} className="capitalize">
+              {subject}
+            </SelectItem>
+          ))}
+
+        </SelectContent>
+      </Select>
+    </section>
+  );
+}
+export default SubjectFilter;
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,17 +1,52 @@
 'use server'
-import  {auth}  from "@clerk/nextjs/server";
+import {auth} from "@clerk/nextjs/server";
 import {createSupabaseClient} from "@/lib/supabase";
 
 export const createCompanion = async (formData: CreateCompanion) => {
-  const { userId: author } = await auth();
+  const {userId: author} = await auth();
   const supabase = createSupabaseClient();
 
-  const { data, error } = await supabase
+  const {data, error} = await supabase
     .from('companions')
-    .insert({...formData, author })
+    .insert({...formData, author})
     .select();
 
-  if(error || !data) throw new Error(error?.message || 'Failed to create a companion');
+  if (error || !data) throw new Error(error?.message || 'Failed to create a companion');
+
+  return data[0];
+}
+
+export const getAllCompanions = async ({limit = 10, page = 1, subject, topic}: GetAllCompanions) => {
+  const supabase = createSupabaseClient();
+
+  let query = supabase.from('companions').select();
+
+  if (subject && topic) {
+    query = query.ilike('subject', `%${subject}%`)
+      .or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`);
+  } else if (subject) {
+    query = query.ilike('subject', `%${subject}%`);
+  } else if (topic) {
+    query = query.ilike('topic', `%${topic}%`);
+  }
+
+
+  query = query.range((page - 1) * limit, page * limit - 1);
+
+  const { data: companions, error } = await query;
+  if(error) throw new Error(error.message)
+
+  return companions;
+}
+
+export const getCompanion = async (id: string | Array<string> | undefined) => {
+  const supabase = createSupabaseClient();
+  const { data, error } = await supabase
+    .from('companions')
+    .select()
+    .eq('id', id);
+
+  if(error) throw new Error(error.message)
 
   return data[0];
 }
